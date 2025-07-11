03b6e9c4e7f776f71c616cbb3165ba22cb7fe601
Dinesh Kumar <dkswdeveloper@gmail.com>
2025-07-11 14:11:23 +0530

    tesing phase 1

--- todo/todo/src/main/java/com/lpu/todo/service/UserServiceImpl.java
+++ todo/todo/src/main/java/com/lpu/todo/service/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.lpu.todo.service;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +13,33 @@ import com.lpu.todo.repo.UserRepository;
 import jakarta.transaction.Transactional;
 
 @Service
-@Transactional
+
 public class UserServiceImpl implements UserService  {
+	private static final Logger log =LoggerFactory.getLogger(UserServiceImpl.class);
 	@Autowired
 	UserRepository userRepo;
 	
 	@Override
 	public User findByUsername(String username)
 	{
-		return userRepo.findById(username).get();
+		log.debug("findByUsername called searching for "+ username);
+		try
+		{
+		Optional<User> optional = userRepo.findById(username);
+		//if there is no element, throw NoSuchEleemntException 
+		return optional.orElse(null); // if found reutrn user, else null
+		}
+		catch(Exception e)
+		{
+			log.error(username + " was not found");
+			return null;
+		}
 	}
 	@Override
+	@Transactional
 	public User addUser(User user)
 	{
+		log.debug("trying to add user " + user.getUsername());
 		userRepo.save(user);
 		return user;
 	}
--- todo/todo/src/test/java/com/lpu/todo/service/UserServiceImplTest.java
+++ todo/todo/src/test/java/com/lpu/todo/service/UserServiceImplTest.java
@@ -1,7 +1,13 @@
 package com.lpu.todo.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.lpu.todo.model.User;
 @SpringBootTest
 class UserServiceImplTest {
+	public static List<String> list = new ArrayList<>();
 	
 	@Autowired
 	UserService userService;
@@ -18,9 +25,29 @@ class UserServiceImplTest {
 	User byUsername = userService.findByUsername("yash");
 	assertNotNull(byUsername);
 	}
+	@Test
+	void testFindByUsernameNotExist() {
+	User byUsername = userService.findByUsername("ayush234");
+	assertNull(byUsername);
+	}
+	@Test
+	void testFindByUsernameNullValue() {
+	User byUsername = userService.findByUsername(null);
+	assertNull(byUsername);
+	}
 
 	@Test
 	void testAddUser() {
+		String username = "renu" + System.currentTimeMillis();
+		User user =new User(username,  "renu", true);
+		userService.addUser(user);
+		list.add(username);
+	}
+	@AfterAll
+	public static void afterAllTests()
+	{
+		// delete the dummy data
+		
 		
 	}
 
